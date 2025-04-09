f45649059fc7a0a22e6054d0f468947d39f287d0
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-09 12:54:51 +0300

    feat: CompanyManager dosyası düzenlenildi.

--- Business/CompanyManager.cs
+++ Business/CompanyManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,24 @@ namespace Teklif_Hazırlayıcı.Business
 {
     public class CompanyManager
     {
+        private readonly DataAccess.DbConnection _connection;
+
+        public CompanyManager()
+        {
+            _connection = new DataAccess.DbConnection();
+        }
+
+        public DataTable GetCompany()
+        {
+            string query = "SELECT * FROM Company ORDER BY Name";
+            using (OleDbCommand cmd = new OleDbCommand(query, _connection.GetConnection()))
+            {
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+
+        }
     }
 }
