da9a9f252d0c45e9c665977291836028a2795352
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-09 10:15:12 +0300

    feat: UserManager.cs dosyasına AddUser fonksiyonu eklendi.

--- Business/UserManager.cs
+++ Business/UserManager.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Teklif_Hazırlayıcı.DataAccess;
+using Teklif_Hazırlayıcı.Helpers;
 
 namespace Teklif_Hazırlayıcı.Business
 {
@@ -16,6 +17,81 @@ namespace Teklif_Hazırlayıcı.Business
         public UserManager()
         {
             _connection = new DataAccess.DbConnection();
+        }
+
+        public bool AddUser(string name, string surname, string username, string email, string password)
+        {
+            if (isThere(username))
+            {
+                MessageHelper.ShowError($"\"{username}\" kullanıcı adı zaten kullanılmaktadır.");
+                return false;
+            }
+            else
+            {
+                if (isThere(email))
+                {
+                    MessageHelper.ShowError($"\"{email}\" e-posta adresi zaten kullanılmaktadır.");
+                    return false;
+                }
+                else
+                {
+                    try
+                    {
+                        using (OleDbConnection conn = _connection.GetConnection())
+                        {
+                            string query = "INSERT INTO kullanicilar(isim,soyisim,kullanici_adi, eposta, parola) VALUES (@Name, @Surname, @Username, @Email, @Password)";
+                            using (OleDbCommand command = new OleDbCommand(query, _connection.GetConnection()))
+                            {
+                                command.Parameters.AddWithValue("@Name", username);
+                                command.Parameters.AddWithValue("@Surname", surname);
+                                command.Parameters.AddWithValue("@Username", username);
+                                command.Parameters.AddWithValue("@Email", email);
+                                command.Parameters.AddWithValue("@Password", password);
+                                int result = command.ExecuteNonQuery();
+                                if (result > 0)
+                                {
+                                    MessageHelper.ShowSuccess("Başarıyla kayıt oldunuz!");
+                                    return true;
+                                }
+                                else
+                                {
+                                    MessageHelper.ShowError("Kayıt işlemi sırasında bilinmeyen bir hatayla karşılaşıldı.");
+                                    return false;
+                                }
+                            }
+                                
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageHelper.ShowError("Hata: " + ex.Message);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private bool isThere(string parameter)
+        {
+            try
+            {
+                _connection.Open();
+                using (OleDbConnection conn = _connection.GetConnection())
+                {
+                    string query = "SELECT COUNT(*) FROM kullanicilar WHERE kullanici_adi = @Parameter";
+                    using (OleDbCommand command = new OleDbCommand(query, _connection.GetConnection()))
+                    {
+                        command.Parameters.AddWithValue("@Parameter", parameter);
+                        int count = (int)command.ExecuteScalar();
+                        return count > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageHelper.ShowError("Hata: " + ex.Message);
+                return false;
+            }
         }
 
         public bool UserExists(string username, string password)
