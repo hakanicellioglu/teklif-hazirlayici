b03389330a2ea470f5de3d7c568b17c5c91618dc
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-09 13:01:04 +0300

    chore: CompanyManager ile veri kaynağı güncellendi

--- Forms/company.cs
+++ Forms/company.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Teklif_Hazırlayıcı.Business;
 
 namespace Teklif_Hazırlayıcı.Forms
 {
@@ -15,6 +16,13 @@ namespace Teklif_Hazırlayıcı.Forms
         public company()
         {
             InitializeComponent();
+            LoadCompany();
+        }
+
+        private void LoadCompany()
+        {
+            CompanyManager manager = new CompanyManager();
+            dataGridView1.DataSource = manager.GetCompany();
         }
     }
 }
