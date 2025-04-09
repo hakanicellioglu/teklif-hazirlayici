b8dc498e62fd3a290f83f2117b09cc0ff384995e
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-09 10:16:16 +0300

    TogglePassword sınıfı ve fonksiyonu eklendi

---
+++ Helpers/TogglePassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teklif_Hazırlayıcı.Helpers
+{
+    public class TogglePassword
+    {
+        public TogglePassword(TextBox textBox, Button button) 
+        {
+            if (textBox.UseSystemPasswordChar)
+            {
+                textBox.UseSystemPasswordChar = false;
+                button.Text = "Gizle";
+            }
+            else
+            {
+                textBox.UseSystemPasswordChar = true;
+                button.Text = "Göster";
+            }
+        }
+    }
+}
