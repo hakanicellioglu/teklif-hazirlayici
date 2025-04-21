ec637c02e1bf427788e52614ad1d955f142ebf2e
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-21 13:15:47 +0300

    style(product): Kalıp numarası ile ürün isminin bulunduğu sütunlar yer değişti.

--- Forms/product.cs
+++ Forms/product.cs
@@ -53,17 +53,18 @@ namespace Teklif_Hazırlayıcı.Forms
             if (dataGridView1.Columns["urun_id"] != null)
                 dataGridView1.Columns["urun_id"].Visible = false;
 
+            if (dataGridView1.Columns["kalip_no"] != null)
+            {
+                dataGridView1.Columns["kalip_no"].DisplayIndex = 1;
+                dataGridView1.Columns["kalip_no"].HeaderText = "Kalıp No";
+            }
+
             if (dataGridView1.Columns["urun"] != null)
             {
                 dataGridView1.Columns["urun"].DisplayIndex = 2;
                 dataGridView1.Columns["urun"].HeaderText = "Ürün";
             }
 
-            if (dataGridView1.Columns["kalip_no"] != null)
-            {
-                dataGridView1.Columns["kalip_no"].DisplayIndex = 1;
-                dataGridView1.Columns["kalip_no"].HeaderText = "Kalıp No";
-            }
 
             if (dataGridView1.Columns["gramaj"] != null)
             {
