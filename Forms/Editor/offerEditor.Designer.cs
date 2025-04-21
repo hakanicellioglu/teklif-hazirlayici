47b641c0257fb32e04d37a597d7ef2a120a40465
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-21 09:55:20 +0300

    style(offerEditor): DataGridView özelleştirmesi için stil örneği eklendi

--- Forms/Editor/offerEditor.Designer.cs
+++ Forms/Editor/offerEditor.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.chkFirmalar = new System.Windows.Forms.ComboBox();
@@ -61,7 +65,10 @@
             this.label15 = new System.Windows.Forms.Label();
             this.txtKDV = new System.Windows.Forms.TextBox();
             this.chkVade = new System.Windows.Forms.ComboBox();
-            this.btnEdit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txtİscilik = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -140,6 +147,7 @@
             // 
             // chkTeslimSekli
             // 
+            this.chkTeslimSekli.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.chkTeslimSekli.FormattingEnabled = true;
             this.chkTeslimSekli.Items.AddRange(new object[] {
             "Fabrika Teslim"});
@@ -159,6 +167,7 @@
             // 
             // chkOdemeSekli
             // 
+            this.chkOdemeSekli.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.chkOdemeSekli.FormattingEnabled = true;
             this.chkOdemeSekli.Items.AddRange(new object[] {
             "Nakit"});
@@ -219,7 +228,7 @@
             this.label10.Location = new System.Drawing.Point(50, 239);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(32, 13);
-            this.label10.TabIndex = 19;
+            this.label10.TabIndex = 21;
             this.label10.Text = "Vade";
             // 
             // label11
@@ -228,7 +237,7 @@
             this.label11.Location = new System.Drawing.Point(50, 278);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(58, 13);
-            this.label11.TabIndex = 21;
+            this.label11.TabIndex = 23;
             this.label11.Text = "LME(0.##)";
             // 
             // txtLME
@@ -236,7 +245,7 @@
             this.txtLME.Location = new System.Drawing.Point(53, 294);
             this.txtLME.Name = "txtLME";
             this.txtLME.Size = new System.Drawing.Size(121, 20);
-            this.txtLME.TabIndex = 22;
+            this.txtLME.TabIndex = 24;
             // 
             // label12
             // 
@@ -244,7 +253,7 @@
             this.label12.Location = new System.Drawing.Point(177, 278);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(71, 13);
-            this.label12.TabIndex = 23;
+            this.label12.TabIndex = 25;
             this.label12.Text = "İskonto(0.##)";
             // 
             // txtIskonto
@@ -252,15 +261,16 @@
             this.txtIskonto.Location = new System.Drawing.Point(180, 294);
             this.txtIskonto.Name = "txtIskonto";
             this.txtIskonto.Size = new System.Drawing.Size(121, 20);
-            this.txtIskonto.TabIndex = 24;
+            this.txtIskonto.TabIndex = 26;
             this.txtIskonto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIskonto_KeyPress);
             // 
             // txtTevkifat
             // 
+            this.txtTevkifat.Enabled = false;
             this.txtTevkifat.Location = new System.Drawing.Point(413, 294);
             this.txtTevkifat.Name = "txtTevkifat";
             this.txtTevkifat.Size = new System.Drawing.Size(121, 20);
-            this.txtTevkifat.TabIndex = 28;
+            this.txtTevkifat.TabIndex = 30;
             // 
             // chkTevkifat
             // 
@@ -268,7 +278,7 @@
             this.chkTevkifat.Location = new System.Drawing.Point(413, 277);
             this.chkTevkifat.Name = "chkTevkifat";
             this.chkTevkifat.Size = new System.Drawing.Size(94, 17);
-            this.chkTevkifat.TabIndex = 27;
+            this.chkTevkifat.TabIndex = 29;
             this.chkTevkifat.Text = "Tevkifat(0.##)";
             this.chkTevkifat.UseVisualStyleBackColor = true;
             this.chkTevkifat.CheckedChanged += new System.EventHandler(this.chkTevkifat_CheckedChanged);
@@ -279,7 +289,7 @@
             this.label13.Location = new System.Drawing.Point(50, 317);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(38, 13);
-            this.label13.TabIndex = 29;
+            this.label13.TabIndex = 31;
             this.label13.Text = "Durum";
             // 
             // chkDurum
@@ -294,24 +304,24 @@
             this.chkDurum.Location = new System.Drawing.Point(53, 333);
             this.chkDurum.Name = "chkDurum";
             this.chkDurum.Size = new System.Drawing.Size(121, 21);
-            this.chkDurum.TabIndex = 30;
+            this.chkDurum.TabIndex = 32;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(429, 356);
+            this.button1.Location = new System.Drawing.Point(432, 369);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 23);
-            this.button1.TabIndex = 31;
+            this.button1.TabIndex = 33;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(429, 385);
+            this.btnCancel.Location = new System.Drawing.Point(324, 369);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(102, 23);
-            this.btnCancel.TabIndex = 32;
+            this.btnCancel.TabIndex = 34;
             this.btnCancel.Text = "İptal";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
@@ -345,7 +355,7 @@
             this.label15.Location = new System.Drawing.Point(304, 278);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(58, 13);
-            this.label15.TabIndex = 25;
+            this.label15.TabIndex = 27;
             this.label15.Text = "KDV(0.##)";
             // 
             // txtKDV
@@ -353,7 +363,7 @@
             this.txtKDV.Location = new System.Drawing.Point(307, 294);
             this.txtKDV.Name = "txtKDV";
             this.txtKDV.Size = new System.Drawing.Size(100, 20);
-            this.txtKDV.TabIndex = 26;
+            this.txtKDV.TabIndex = 28;
             // 
             // chkVade
             // 
@@ -364,25 +374,80 @@
             this.chkVade.Location = new System.Drawing.Point(53, 255);
             this.chkVade.Name = "chkVade";
             this.chkVade.Size = new System.Drawing.Size(121, 21);
-            this.chkVade.TabIndex = 20;
+            this.chkVade.TabIndex = 22;
             // 
-            // btnEdit
+            // dataGridView1
             // 
-            this.btnEdit.Location = new System.Drawing.Point(430, 327);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(100, 23);
-            this.btnEdit.TabIndex = 33;
-            this.btnEdit.Text = "Ürünleri Düzenle";
-            this.btnEdit.UseVisualStyleBackColor = true;
-            this.btnEdit.Visible = false;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeight = 50;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 398);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 50;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(484, 241);
+            this.dataGridView1.TabIndex = 35;
+            this.dataGridView1.Visible = false;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(177, 200);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(33, 13);
+            this.label16.TabIndex = 19;
+            this.label16.Text = "İşçilik";
+            // 
+            // txtİscilik
+            // 
+            this.txtİscilik.Location = new System.Drawing.Point(180, 216);
+            this.txtİscilik.Name = "txtİscilik";
+            this.txtİscilik.Size = new System.Drawing.Size(100, 20);
+            this.txtİscilik.TabIndex = 20;
             // 
             // offerEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 461);
-            this.Controls.Add(this.btnEdit);
+            this.ClientSize = new System.Drawing.Size(584, 689);
+            this.Controls.Add(this.txtİscilik);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.chkVade);
             this.Controls.Add(this.txtKDV);
             this.Controls.Add(this.label15);
@@ -416,10 +481,14 @@
             this.Controls.Add(this.chkFirmalar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "offerEditor";
             this.Padding = new System.Windows.Forms.Padding(50);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Teklif Hazırlayıcı";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -460,6 +529,8 @@
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox txtKDV;
         private System.Windows.Forms.ComboBox chkVade;
-        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox txtİscilik;
     }
 }
