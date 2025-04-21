63efd396b6cd0e3c792ee2058a9badf69c9cff89
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-04-21 10:55:47 +0300

    feat(offerEditor): DataGridView hücresine çift tıklama olayı eklendi

--- Forms/Editor/offerEditor.Designer.cs
+++ Forms/Editor/offerEditor.Designer.cs
@@ -423,6 +423,7 @@
             this.dataGridView1.Size = new System.Drawing.Size(484, 241);
             this.dataGridView1.TabIndex = 35;
             this.dataGridView1.Visible = false;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // label16
             // 
