044d41c7c332d098b0da8345402def6da01b5c9e
Hakan Berke İçellioğlu <hakanicellioglu@gmail.com>
2025-06-18 11:35:23 +0300

    Yeni ColumnForm sınıfı ve bileşenleri eklendi
    ColumnForm.Designer.cs dosyasında yeni bir `ColumnForm` sınıfı tanımlandı. Formda bir etiket (`label1`) ve bir kontrol listesi kutusu (`checkedListBox1`) eklendi. Formun görünümü ve davranışları `InitializeComponent` metodu içinde ayarlandı.
    
    ColumnForm.cs dosyasında, `ColumnForm` sınıfının yapıcısı eklendi ve bileşenlerin başlatılması sağlandı.
    
    ColumnForm.resx dosyasında, formun simgesi için gerekli kaynak verileri eklendi.

---
+++ Forms/Custom Item/ColumnForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TeklifHazirlayici.Forms.Custom_Item
+{
+    partial class ColumnForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColumnForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sütunları Düzenle";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(20, 43);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(304, 538);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // ColumnForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 601);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ColumnForm";
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.Text = "Teklif Hazırlayıcı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
