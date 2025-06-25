33d5cb622926643adccefd0e30a5461b218c0ab8
Hakan Berke İçellioğlu <hakanicellioglu@gmail.com>
2025-06-25 14:38:36 +0300

    Sürüm 2.0.0.1 güncellemeleri ve kaynak dosya değişiklikleri
    `offer.resx` dosyasında `imgButton.ImageStream` öğesinin ikili veri değeri güncellendi.
    
    `AssemblyInfo.cs` ve `version.txt` dosyalarında sürüm numarası `1.0.0.19`'dan `2.0.0.1`'e yükseltildi. Yeni sürümle ilgili bağlantı eklendi.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.19")]
-[assembly: AssemblyFileVersion("1.0.0.19")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 
